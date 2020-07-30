d24fc7f7912ab6b9181dc4f9b3d563b0954fc1e5
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-30 19:54:32 -0400

    finished client testing

--- api/clients/clientModel.spec.js
+++ api/clients/clientModel.spec.js
@@ -2,16 +2,33 @@ const db = require("../../data/dbConfig");
 const supertest = require("supertest");
 const server = require("../server");
 
+beforeEach(async () => {
+  return await db.migrate
+    .rollback()
+    .then(() => db.migrate.latest())
+    .then(() => db.seed.run());
+});
+
 describe("environment", function () {
   it("should be using the testing database", function () {
     expect(process.env.DB_ENV).toBe("testing");
   });
 
+  //register a client
+  describe("register a client", function () {
+    //Client be able to login and retrieve the class the client enrolled
+    it("login and get all the classes ", () => {
+      return supertest(server)
+        .post("/api/auth/register")
+        .send({ username: "weipeluso", password: "123456789", role: "client" })
+        .then((res) => {
+          expect(res.status).toEqual(201);
+        });
+    });
+  });
+
   describe("client model", function () {
     //Client be able to login and retrieve the class the client enrolled
-    beforeEach(async () => {
-      await db("userclass").truncate();
-    });
     it("login and get all the classes ", () => {
       return supertest(server)
         .post("/api/auth/login")
@@ -61,64 +78,63 @@ describe("environment", function () {
             });
         });
     });
-  });
-
-  //check if a client can add a class
-  it("login and add a class ", () => {
-    return supertest(server)
-      .post("/api/auth/login")
-      .send({ username: "LambdaClient", password: "12345678" })
-      .then((res) => {
-        const token = res.body.token;
-        return supertest(server)
-          .post("/api/client/classes/8")
-          .set("authorization", token)
-          .then((res) => {
-            supertest(server)
-              .get("/api/client/classes")
-              .set("authorization", token)
-              .then((res) => {
-                const classes = res.body.data;
-                expect(classes).toHaveLength(1);
-              });
-          });
-      });
-  });
-  //check if no of the client enrolled class is 3
-  it("login and get all the classes ", () => {
-    return supertest(server)
-      .post("/api/auth/login")
-      .send({ username: "LambdaClient", password: "12345678" })
-      .then((res) => {
-        const token = res.body.token;
-        return supertest(server)
-          .get("/api/client/classes")
-          .set("authorization", token)
-          .then((res) => {
-            const classes = res.body.data;
-            expect(classes).toHaveLength(1);
-          });
-      });
-  });
-});
-//check if a client can remove a class
-it("login and get all the classes ", () => {
-  return supertest(server)
-    .post("/api/auth/login")
-    .send({ username: "LambdaClient", password: "12345678" })
-    .then((res) => {
-      const token = res.body.token;
+    //check if the no of clinet enrolled classes is 3
+    it("login and get all the classes ", () => {
       return supertest(server)
-        .delete("/api/client/classes/8")
-        .set("authorization", token)
+        .post("/api/auth/login")
+        .send({ username: "LambdaClient", password: "12345678" })
         .then((res) => {
-          supertest(server)
-            .get("/api/client/classes")
+          const token = res.body.token;
+          return supertest(server)
+            .get("/api/client/classes/")
             .set("authorization", token)
             .then((res) => {
               const classes = res.body.data;
-              expect(classes).toHaveLength(0);
+              expect(classes).toHaveLength(3);
             });
         });
     });
+    //check if a client can add a class and no of class become 4
+    it("login and add a class ", () => {
+      return supertest(server)
+        .post("/api/auth/login")
+        .send({ username: "LambdaClient", password: "12345678" })
+        .then((res) => {
+          const token = res.body.token;
+          return supertest(server)
+            .post("/api/client/classes/8")
+            .set("authorization", token)
+            .then((res) => {
+              supertest(server)
+                .get("/api/client/classes")
+                .set("authorization", token)
+                .then((res) => {
+                  const classes = res.body.data;
+                  expect(classes).toHaveLength(4);
+                });
+            });
+        });
+    });
+    //check if a client can remove a class and the class no become 3
+    it("login and get all the classes ", () => {
+      return supertest(server)
+        .post("/api/auth/login")
+        .send({ username: "LambdaClient", password: "12345678" })
+        .then((res) => {
+          const token = res.body.token;
+          return supertest(server)
+            .delete("/api/client/classes/8")
+            .set("authorization", token)
+            .then((res) => {
+              supertest(server)
+                .get("/api/client/classes")
+                .set("authorization", token)
+                .then((res) => {
+                  const classes = res.body.data;
+                  expect(classes).toHaveLength(3);
+                });
+            });
+        });
+    });
+  });
 });
